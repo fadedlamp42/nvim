1d0cbc0354df6e494715ad45f846f666d07ed584
Peter Gomez <fadedlamp42@gmail.com>
2021-06-21 01:15:12 -0400

    disable autochdir for better grep

--- lua/options.lua
+++ lua/options.lua
@@ -10,7 +10,7 @@ o.showmatch = true 			-- briefly jump to matching [{()}] on completion
 o.lazyredraw = true 			-- don't update screen during macros
 
 -- functional
-o.autochdir = true 			-- working directory is always directory of current file
+--o.autochdir = true 			-- working directory is always directory of current file
 o.hidden = true 			-- don't close abandoned buffers (allows background persistence)
 o.completeopt = 'menuone,noselect' 	-- necessary for compe
 o.mouse = 'a' 				-- mouse support
